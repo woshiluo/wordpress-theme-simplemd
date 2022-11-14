82ebce87c43e1804fe6359858dd58ef4adb928cf
Woshiluo Luo <woshiluo.luo@outlook.com>
2022-11-14 12:51:14 +0800

    fix: lightgallery wrong source when image is not from wordpress's media library

--- js/customizer.js
+++ js/customizer.js
@@ -19,30 +19,32 @@ if ('serviceWorker' in navigator) {
 
 jQuery(document).ready( function(){
 	// Light Gallery
-	let lightgallery_list = Array(), images = Array();
-	jQuery(".entry-content img").each( function() {
+	jQuery(".entry-content img").each( () => {
+		const lightgallery_list = [];
 		jQuery(this).css('cursor', 'pointer');
-		let element = jQuery(this).siblings("figcaption");
-		let img_alt = jQuery(element).text();
-		lightgallery_list.push({ "src": this.srcset.split(' ').reverse()[1], "thumb": this.src, "subHtml": img_alt + "<br/>By Woshiluo"});
+		const element = jQuery(this).siblings("figcaption");
+		const img_alt = jQuery(element).text();
+		const origin_source = this.srcset.split(' ').reverse()[1];
+		lightgallery_list.push({ "src": ( origin_source === undefined? this.src: origin_source ) , "thumb": this.src, "subHtml": img_alt + "<br/>By Woshiluo"});
+
+		const images = jQuery(".entry-content img");
+		for( let i = 0; i < images.length; i ++ ) {
+			images[i].now = i;
+			jQuery(images[i]).click( () => {
+				const idx = this.now;
+				console.log( idx );
+				jQuery("body").lightGallery({
+					autoplay: false,
+					autoplayControls: false,
+					share: false,
+					counter: true,
+					dynamic: true,
+					download: false,
+					dynamicEl: lightgallery_list
+				});
+				jQuery("body").data('lightGallery').index = idx;
+			});
+		}
 	});
 
-	images = jQuery(".entry-content img");
-	for( let i = 0; i < images.length; i ++ ) {
-		images[i].now = i;
-		jQuery(images[i]).click( function() {
-			let idx = this.now;
-			console.log( idx );
-			jQuery("body").lightGallery({
-				autoplay: false,
-				autoplayControls: false,
-				share: false,
-				counter: true,
-				dynamic: true,
-				download: false,
-				dynamicEl: lightgallery_list
-			});
-			jQuery("body").data('lightGallery').index = idx;
-		});
-	}
 });
